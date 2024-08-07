3afd7dfcfdab1588154907c23c6953bbf78458a5
kefflen <kefflenjava@hotmail.com>
2024-08-07 12:57:51 -0300

    feat: Add create button when title and content filled

--- src/app/components/CreateNoteCard/index.tsx
+++ src/app/components/CreateNoteCard/index.tsx
@@ -24,29 +24,39 @@ export const CreateNoteCard = () => {
 
   return (
     <Card className="sm:w-[530px] h-[100px]">
-      <form onSubmit={onSubmit}>
-        <div className="flex px-5">
-          <input
-            type="text"
-            placeholder="Titulo..."
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            className="flex-1 text-xl font-bold py-2 outline-none"
-          />
-          <FavoriteToggleIcon
-            isFavorite={isFavorite}
-            setIsFavorite={setIsFavorite}
-          />
+      <form onSubmit={onSubmit} className="flex flex-col gap-4">
+        <div>
+          <div className="flex px-5">
+            <input
+              type="text"
+              placeholder="Titulo..."
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className="flex-1 text-xl font-bold py-2 outline-none"
+            />
+            <FavoriteToggleIcon
+              isFavorite={isFavorite}
+              setIsFavorite={setIsFavorite}
+            />
+          </div>
+          <hr />
+          <div className="flex px-5">
+            <textarea
+              placeholder="Criar nota..."
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              className="flex-1 outline-none resize-none res"
+            />
+          </div>
         </div>
-        <hr />
-        <div className="flex px-5">
-          <textarea
-            placeholder="Criar nota..."
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-            className="flex-1 outline-none resize-none res"
-          />
-        </div>
+        {title && content && (
+          <button
+            type="submit"
+            className=" bg-emerald-400 p-2 rounded-md self-end shadow-lg"
+          >
+            Criar
+          </button>
+        )}
       </form>
     </Card>
   )
